74b82aa8a0a251ec915258d7ebcd90312ac13a82
Éric Ivan João António <ericantoniodev@gmail.com>
2022-10-31 14:35:10 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Task from './components/task';
 
 export default function App() {
   return (
@@ -10,9 +12,12 @@ export default function App() {
 
         <View style={styles.items}>
           {/* This is where the tasks will go! */}
+          <Task/>
         </View>
 
       </View>
+
+
     </View>
   );
 }
