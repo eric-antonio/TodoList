4d60cd5959f1a6a1231cd49c98ea5174de9553bf
Éric Ivan João António <ericantoniodev@gmail.com>
2022-10-31 14:41:32 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Task from './components/task';
+import Task from './components/Task';
 
 export default function App() {
   return (
