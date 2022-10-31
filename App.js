681ea9e278211ec6553ae7d3d1028d4c626cb278
Éric Ivan João António <ericantoniodev@gmail.com>
2022-10-31 14:19:56 +0200

    Update App.js

--- App.js
+++ App.js
@@ -4,8 +4,15 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Olá Mundo! React Native Let's Go!</Text>
-      <StatusBar style="auto" />
+      {/* Today's Tasks */}
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}>Today's tasks</Text>
+
+        <View style={styles.items}>
+          {/* This is where the tasks will go! */}
+        </View>
+
+      </View>
     </View>
   );
 }
@@ -13,8 +20,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#E8EAED',
   },
+  
+  tasksWrapper: {
+     
+    paddingTop: 80,
+    paddingHorizontal:20,
+  },
+  sectionTitle: {},
+  items: {},
 });
