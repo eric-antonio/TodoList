e82dcb2d07b197fdbabeff51acdd616f48ec57a4
Éric Ivan João António <ericantoniodev@gmail.com>
2022-10-31 14:30:52 +0200

    Update task.js

--- components/task.js
+++ components/task.js
@@ -1,2 +1,12 @@
 import React from "react";
 import { View , Text, StyleSheet} from "react-native";
+const Task = ()=>{
+
+    return(
+        <View style={styles.cotainerTasks}>
+        <Text style={styles.Tarefas}>Começar a ver ReactNative!</Text>
+        </View>
+    )
+}
+
+export default Task;
